2df448a656ec38ecbd3f59324f8b975bb515d260
primespace <primespace@naver.com>
2016-07-07 00:31:41 +0900

    .

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.apache.commons.dbcp2.ConnectionFactory;
@@ -25,14 +27,57 @@ public class Main {
 	
 		ConnectionPool pool = ConnectionPool.getInstance();
 		
-		pool.set("localhost", 3306, "fanluv", "root", "test1234");
+		pool.set("localhost", 3306, "fanluv", "root", "dlstkdv1");
 		
 		if(pool.startup() == false) {
 			return;
 		}
 		
-		Connection conn = pool.getConnection();
-		pool.closeConnection(conn);;
+		for(int i = 0; i < 100; ++i) {
+			test1();
+		}
 		
+	}
+	
+	static void test1()
+	{
+		ConnectionPool pool = ConnectionPool.getInstance();
+		Connection conn = pool.getConnection();
+		
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			String sql = "select name from gender_type where gender_type = ?";
+			
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, 1);
+			rs = stmt.executeQuery();
+			
+			while(rs.next()) {
+				String name = rs.getString("name");
+				System.out.println("name " + name);
+			}
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			rs.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			stmt.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		pool.closeConnection(conn);
 	}
 }
