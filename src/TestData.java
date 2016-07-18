a0b96f39424efe992316895d98f8750239f927e6
primespace <primespace@naver.com>
2016-07-19 00:42:24 +0900

    .

--- src/TestData.java
+++ src/TestData.java
@@ -2,16 +2,34 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Random;
 
 import com.prepeo.jdbc.ConnectionPool;
+import com.prepeo.jdbc.Helper;
 
 public class TestData {
+	
+	private int[] spaceIds = new int[] {
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+	};
 
 	public void insertRandom() {
 
 		int rankId = getRankId(0);
-		System.out.println("rankId " + rankId);
 		
+		Random rand = new Random();
+		
+		for(int spaceId : spaceIds) {
+						
+			insertHeart(rankId, spaceId, rand.nextInt(100000));
+		}
+	}
+	
+	private void insertHeart(int rankId, int spaceId, int nextInt) {
+
+		if(isExist(rankId, spaceId)) {
+			
+		}
 		
 	}
 	
@@ -23,8 +41,7 @@ public class TestData {
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
 		
-		String sql = "select fn_toDays(date_add(now(), interval ? day)) rankId from dual";
-		// String sql = "select fn_toWeeks(date_add(now(), interval ? day)) rankId from dual";
+		String sql = "select fn_getRankId(?) rankId from dual";
 		
 		int rankId = 0;
 		
@@ -42,19 +59,8 @@ public class TestData {
 			e.printStackTrace();
 		}
 		
-		try {
-			rs.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		try {
-			stmt.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		Helper.close(rs);
+		Helper.close(stmt);
 		
 		pool.closeConnection(conn);
 		
@@ -63,10 +69,64 @@ public class TestData {
 	
 	private boolean isExist(final int rankId, final int spaceId) {
 		
-		return false;
+		ConnectionPool pool = ConnectionPool.getInstance();
+		Connection conn = pool.getConnection();
+		
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		int count = 0;
+		try {
+			String sql = "select count(rank_id) from space_rank_heart where rank_id = ? and space_id = ? ";
+			
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1,  rankId);
+			stmt.setInt(2, spaceId);
+			
+			while(rs.next()) {
+				count = rs.getInt(1);
+			}
+						
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		Helper.close(rs);
+		Helper.close(stmt);
+		pool.closeConnection(conn);
+		
+		
+		return (count > 0);
 	}
 	
-	private void insertTestData(final int spaceId, final int heartCount)
+	private void updateTestData(final int rankId, final int spaceId, final int heartCount)
+	{
+		ConnectionPool pool = ConnectionPool.getInstance();
+		Connection conn = pool.getConnection();
+		
+		PreparedStatement stmt = null;
+		
+		try {
+			
+			String sql = "update space_rank_heart set heart_count = ? where rank_id = ? and spaceId = ? ";
+			
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, heartCount);
+			stmt.setInt(2, rankId);
+			stmt.setInt(3, spaceId);
+
+			stmt.executeUpdate();
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		Helper.close(stmt);
+	
+		pool.closeConnection(conn);
+	}
+	
+	private void insertTestData(final int rankId, final int spaceId, final int heartCount)
 	{
 		ConnectionPool pool = ConnectionPool.getInstance();
 		Connection conn = pool.getConnection();
@@ -76,14 +136,15 @@ public class TestData {
 		try {
 			
 			String sql = "insert into space_rank_heart(rank_id, space_id, heart_count, rank, rank_up, dense_rank) ";
-			sql += "values(fn_firstWeekToDays(now()), ?, ?, ?, ?, ?)";
+			sql += "values(?, ?, ?, ?, ?, ?)";
 			
 			stmt = conn.prepareStatement(sql);
-			stmt.setInt(1, 1);
-			stmt.setInt(2, 1);
-			stmt.setInt(3, 1);
-			stmt.setInt(4, 1);
-			stmt.setInt(5, 1);
+			stmt.setInt(1, rankId);
+			stmt.setInt(2, spaceId);
+			stmt.setInt(3, heartCount);
+			stmt.setInt(4, 0);
+			stmt.setInt(5, 0);
+			stmt.setInt(6, 0);
 
 			stmt.executeUpdate();
 			
@@ -91,13 +152,8 @@ public class TestData {
 			e.printStackTrace();
 		}
 		
-		try {
-			stmt.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		Helper.close(stmt);
+	
 		pool.closeConnection(conn);
 	}
 
---
+++ src/com/prepeo/jdbc/Helper.java
@@ -0,0 +1,26 @@
+package com.prepeo.jdbc;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Helper {
+
+	public static void close(ResultSet rs) {
+		try {
+			rs.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(Statement stmt) {
+		try {
+			stmt.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
