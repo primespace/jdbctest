aeb193fdd278c4615f5253daf81f846dc045c7eb
primespace <primespace@naver.com>
2016-07-24 15:42:02 +0900

    .

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 import org.apache.commons.dbcp2.ConnectionFactory;
 import org.apache.commons.dbcp2.PoolableConnection;
 import org.apache.commons.dbcp2.PoolableConnectionFactory;
@@ -23,7 +26,11 @@ import com.prepeo.jdbc.ConnectionPool;
  */
 public class Main {
 	
+	public static ArrayList<Integer> rankIds = new ArrayList<>();
+
 	public static void main(String[] args) {
+		
+		System.out.println(new Date().toString());
 	
 		ConnectionPool pool = ConnectionPool.getInstance();
 		
@@ -33,38 +40,59 @@ public class Main {
 			return;
 		}
 		
-		TestData data = new TestData();
-		data.insertRandom();
+		selectRankIds();
 		
-//		for(int i = 0; i < 100; ++i) {
-//			test1();
-//		}
+		int curRankId = 0;
+		int preRankId = 0;
 		
-//		insertTestData();
 		
+		
+		if(rankIds.size() == 0) {
+			return;
+		}
+		
+		if(rankIds.size() == 1) {
+			curRankId = 0;
+		}
+		else {
+			curRankId = rankIds.get(0);
+			preRankId = rankIds.get(1);
+		}
+		
+		System.out.println(" " + curRankId + " " + preRankId);
+		
+		if(preRankId != 0) {
+			if(isCompleteRankCalc(preRankId) == false) {
+				calcRank(preRankId);
+			}
+		}
+		
+		if(curRankId != 0) {
+			if(isCompleteRankCalc(curRankId) == false) {
+				calcRank(curRankId);
+				if(preRankId != 0) {
+					calcRankUp(curRankId, preRankId);
+				}
+			}
+		}
+		
+		System.out.println(new Date().toString());
 	}
 	
-	static void insertTestData()
-	{
-		System.out.println("insertTestData");
-		
+	private static void calcRankUp(int curRankId, int preRankId) {
 		ConnectionPool pool = ConnectionPool.getInstance();
 		Connection conn = pool.getConnection();
 		
 		PreparedStatement stmt = null;
 		
 		try {
-			
-			String sql = "insert into space_rank_heart(rank_id, space_id, heart_count, rank, rank_up, dense_rank) ";
-			sql += "values(fn_firstWeekToDays(now()), ?, ?, ?, ?, ?)";
+			String sql = "call sp_calcSpaceRankUp(@ret, ?, ?)";
 			
 			stmt = conn.prepareStatement(sql);
-			stmt.setInt(1, 1);
-			stmt.setInt(2, 1);
-			stmt.setInt(3, 1);
-			stmt.setInt(4, 1);
-			stmt.setInt(5, 1);
-
+			
+			stmt.setInt(1, preRankId);
+			stmt.setInt(2, curRankId);
+			
 			stmt.executeUpdate();
 			
 		} catch(SQLException e) {
@@ -79,28 +107,64 @@ public class Main {
 		}
 		
 		pool.closeConnection(conn);
+		
 	}
-	
-	static void test1()
-	{
+
+	private static void calcRank(int rankId) {
+
+		ConnectionPool pool = ConnectionPool.getInstance();
+		Connection conn = pool.getConnection();
+		
+		PreparedStatement stmt = null;
+		
+		try {
+			String sql = "call sp_calcSpaceRank(@ret, ?)";
+			
+			stmt = conn.prepareStatement(sql);
+			
+			stmt.setInt(1, rankId);
+			
+			stmt.executeUpdate();
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			stmt.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		pool.closeConnection(conn);
+		
+	}
+
+	private static boolean isCompleteRankCalc(Integer rankId) {
+
 		ConnectionPool pool = ConnectionPool.getInstance();
 		Connection conn = pool.getConnection();
 		
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
 		
+		int count = 0;
+		
 		try {
-			
-			String sql = "select name from gender_type where gender_type = ?";
+			String sql = "select count(*) from space_rank_complete where rank_id = ?";
 			
 			stmt = conn.prepareStatement(sql);
-			stmt.setInt(1, 1);
+			
+			stmt.setInt(1, rankId);
+			
 			rs = stmt.executeQuery();
 			
-			while(rs.next()) {
-				String name = rs.getString("name");
-				System.out.println("name " + name);
+			if(rs.next() == false) {
+				System.out.println("error !");
 			}
+			
+			count = rs.getInt(1);
 			
 		} catch(SQLException e) {
 			e.printStackTrace();
@@ -121,5 +185,51 @@ public class Main {
 		}
 		
 		pool.closeConnection(conn);
+		
+		
+		return count > 0;
+	}
+
+	public static void selectRankIds()
+	{
+		ConnectionPool pool = ConnectionPool.getInstance();
+		Connection conn = pool.getConnection();
+		
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			String sql = "select fn_getRankId(-1), fn_getRankId(-2) from dual";
+			
+			stmt = conn.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			
+			if(rs.next() == false) {
+				System.out.println("error !");
+			}
+			
+			rankIds.add(rs.getInt(1));
+			rankIds.add(rs.getInt(2));
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			rs.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			stmt.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		pool.closeConnection(conn);
+		
 	}
 }
